a938f904dd57fcca16607086fd38574414d00f0f
Rex St John <rstjohn@Rex-St-John--Intel.local>
2014-05-03 14:58:15 -0700

    first iphone

---
+++ iphone/iCount/UGMPersonTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  UGMPersonTableViewController.h
+//  iCount
+//
+//  Created by Rex St John on 5/3/14.
+//  Copyright (c) 2014 Union Gospel Mission. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UGMPersonTableViewController : UITableViewController
+
+@end
---
+++ iphone/iCount/UGMReportViewcontroller.h
@@ -0,0 +1,13 @@
+//
+//  UGMReportViewcontroller.h
+//  iCount
+//
+//  Created by Rex St John on 5/3/14.
+//  Copyright (c) 2014 Union Gospel Mission. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UGMReportViewcontroller : UIViewController
+
+@end
