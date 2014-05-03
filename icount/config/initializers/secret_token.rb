c88910d76d36b6f4027b5a55170089aa25320c73
Katie <katie@avvo.com>
2014-05-03 13:51:21 -0700

    initial commit

---
+++ icount/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Icount::Application.config.secret_key_base = '78b803081889bf95d2506c2e06f8944a543c1bcb6fc3cdfc0d692d0c2130ac04a79852bcd10124c689ba94d26b19602e4605036e29aa9875edd005a32c89776b'
---
+++ icount/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Icount::Application.config.session_store :cookie_store, key: '_icount_session'
