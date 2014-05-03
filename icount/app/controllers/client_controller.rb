08e65cf26520de8dcdca79b2dcb38a90ab658379
Katie <katie@avvo.com>
2014-05-03 16:30:22 -0700

    simple form for gem and form in client

--- icount/app/controllers/client_controller.rb
+++ icount/app/controllers/client_controller.rb
@@ -1,8 +1,16 @@
 class ClientController < ApplicationController
+
   def new
+    @client = Client.new
   end
 
   def create
+    @client = Client.new(client_params)
+    if @client.save
+      redirect_to main_index_path
+    else 
+      render :action => :new
+    end
   end
 
   def index
@@ -13,4 +21,11 @@ class ClientController < ApplicationController
 
   def update
   end
+
+  protected
+  
+  def client_params
+    params.require(:client).permit(:name, :description)
+  end
+
 end
