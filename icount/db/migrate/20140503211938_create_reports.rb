6d8efe882826e2d298e7fc582872c4bbd6b91270
Katie <katie@avvo.com>
2014-05-03 14:27:25 -0700

    client & report models

---
+++ icount/db/migrate/20140503211938_create_reports.rb
@@ -0,0 +1,13 @@
+class CreateReports < ActiveRecord::Migration
+  def change
+    create_table :reports do |t|
+      t.references :client
+      t.string :location
+      t.string :resources
+      t.string :services
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
